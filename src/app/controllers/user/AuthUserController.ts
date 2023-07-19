ccb2eeadc8d2e17d377b8c86c264913dd21a60e9
Franklin <franklinteixeira205@gmail.com>
2023-07-19 17:25:59 -0300

    feat: endpoint for authenticate user

---
+++ src/app/controllers/user/AuthUserController.ts
@@ -0,0 +1,49 @@
+import repos from "../../../infra/database";
+import { encryptor } from "../../../utils/encryptor";
+import { validSchema } from "../../../validators";
+import { Controller, HttpDataResponse, HttpRequest, HttpResponse } from "../../protocols/controller";
+import { randomAuthToken } from "./services/auth-token";
+import { sendVerificationMail } from "./services/verification-mail";
+
+export class AuthUserController implements Controller {
+
+  async handler(req: HttpRequest, res: HttpResponse<HttpDataResponse>): Promise<HttpResponse> {
+    const { email, password } = req.body;
+
+    const validationUser = await validSchema("user:auth", { email, password });
+    if (validationUser.error) {
+      return res.status(400).json({
+        error: validationUser.error,
+      });
+    }
+
+    const existsUser = await repos.user.findByEmail({ email });
+    if (!existsUser) {
+      return res.status(404).json({
+        error: "Not found user by email"
+      });
+    }
+
+    if (!existsUser.verified) {
+      const actualDate = new Date();
+      const betweenDate =  actualDate.getTime() - existsUser.createdAt.getTime();
+
+      if (betweenDate > 300000) sendVerificationMail(existsUser);
+      return res.status(401).json({
+        error: "Valid your account, check your email"
+      });
+    }
+
+    const comparePass = await encryptor.compare(password, existsUser.password);
+    if (!comparePass) {
+      return res.status(401).json({
+        error: "Invalid password"
+      });
+    }
+
+    const token = randomAuthToken(existsUser);
+    return res.json({
+      data: { token }
+    });
+  }
+}
--- src/main/config/router.ts
+++ src/main/config/router.ts
@@ -3,11 +3,14 @@ import { CreateUserController } from "../../app/controllers/user/CreateUserContr
 import { handlerExpressError } from "./handlers/express-error";
 import "express-async-errors";
 import { ActiveUserController } from "../../app/controllers/user/ActiveUserController";
+import { AuthUserController } from "../../app/controllers/user/AuthUserController";
 
 const router = Router();
 const createUser = new CreateUserController();
 const activeUser = new ActiveUserController();
+const authUser = new AuthUserController();
 
+router.post("/signin", authUser.handler);
 router.post("/signup", createUser.handler);
 router.post("/account/active/:token", activeUser.handler);
 router.use(handlerExpressError);
