31700bf6c3c29dd82a2786f2af7ab44b1361dfcb
Franklin <franklinteixeira205@gmail.com>
2023-07-19 17:35:12 -0300

    refactor: router

--- src/main/config/router.ts
+++ src/main/config/router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { CreateUserController } from "../../app/controllers/user/CreateUserController";
-import { handlerExpressError } from "./handlers/express-error";
 import "express-async-errors";
+
 import { ActiveUserController } from "../../app/controllers/user/ActiveUserController";
 import { AuthUserController } from "../../app/controllers/user/AuthUserController";
+import { CreateUserController } from "../../app/controllers/user/CreateUserController";
+import { handlerExpressError } from "./handlers/express-error";
 
 const router = Router();
+
 const createUser = new CreateUserController();
 const activeUser = new ActiveUserController();
 const authUser = new AuthUserController();
@@ -13,6 +15,7 @@ const authUser = new AuthUserController();
 router.post("/signin", authUser.handler);
 router.post("/signup", createUser.handler);
 router.post("/account/active/:token", activeUser.handler);
+
 router.use(handlerExpressError);
 
 export default router;
