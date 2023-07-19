ac47413f78be82a488e643d576c8461b6229b92f
Franklin <franklinteixeira205@gmail.com>
2023-07-19 12:53:14 -0300

    feat: valid user schema body

--- src/app/controllers/user/CreateUserController.ts
+++ src/app/controllers/user/CreateUserController.ts
@@ -1,9 +1,22 @@
-import { Controller, HttpRequest, HttpResponse, HttpDataResponse } from "../../protocols/controller";
+import { validSchema } from "../../../validators";
+import { Controller, HttpDataResponse, HttpRequest, HttpResponse } from "../../protocols/controller";
 
 export class CreateUserController implements Controller {
   
   async handler(req: HttpRequest, res: HttpResponse<HttpDataResponse>): Promise<HttpResponse> {
     const { username, email, password } = req.body;
+
+    const validationUser = await validSchema("user:create", {
+      username,
+      email,
+      password
+    });
+
+    if(validationUser.error)  {
+      return res.status(400).json({
+        error: validationUser.error
+      });
+    }
 
     return res.json({
       data: {
