66d44e459aa0ec1c3e051b6d5bde20ec4b97445e
Franklin <franklinteixeira205@gmail.com>
2023-07-20 14:49:59 -0300

    fix: ensure variable slug

--- prisma/migrations/20230720174836_feat_models/migration.sql
+++ prisma/migrations/20230720174836_feat_models/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE "tb_user" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tb_user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "tb_board" (
     "id" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
@@ -19,7 +32,10 @@ CREATE TABLE "_board_user_contributing" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "tb_board_slug_key" ON "tb_board"("slug");
+CREATE UNIQUE INDEX "tb_user_username_key" ON "tb_user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tb_user_email_key" ON "tb_user"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_board_user_contributing_AB_unique" ON "_board_user_contributing"("A", "B");
