a5599172d0ba75fa6a16ea8d42837df90d288737
Franklin <franklinteixeira205@gmail.com>
2023-07-20 15:06:45 -0300

    feat: create board enpoint

--- src/app/controllers/board/CreateBoardController.ts
+++ src/app/controllers/board/CreateBoardController.ts
@@ -1,11 +1,38 @@
+import repos from "../../../infra/database";
+import { validSchema } from "../../../validators";
 import { Controller, HttpDataResponse, HttpRequest, HttpResponse } from "../../protocols/controller";
+import { BoardDTO } from "../../dtos/BoardDTO";
+import { toSlug } from "../../../utils/slug";
 
 export class CreateBoardController implements Controller {
-  
+
   async handler(req: HttpRequest, res: HttpResponse<HttpDataResponse>): Promise<HttpResponse> {
-    const { email } = req.auth_user!.data;
-    return res.status(201).json({
-      data: { email }
+    
+    const { title, description } = req.body;
+    const { data: current_user } = req.auth_user!;
+
+    const validationBoard = await validSchema("board:create", {
+      title,
+      description,
+      ownerId: current_user.id
+    });
+
+
+    if (validationBoard.error) {
+      return res.status(400).json({
+        error: validationBoard.error
+      });
+    }
+
+    const board = await repos.board.create({
+      ownerId: current_user.id,
+      title,
+      description,
+      slug: toSlug(title)
+    });
+
+    return res.json({
+      data: BoardDTO.from(board)
     });
   }
 
