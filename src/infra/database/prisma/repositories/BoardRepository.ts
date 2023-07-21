e6bf58da228f830bfd91d07bb539ce2d3d1cefb4
Franklin <franklinteixeira205@gmail.com>
2023-07-21 09:09:07 -0300

    feat: implementation method update with orm prisma

--- src/infra/database/prisma/repositories/BoardRepository.ts
+++ src/infra/database/prisma/repositories/BoardRepository.ts
@@ -1,9 +1,9 @@
 import { BoardModel } from "../../../../domain/models/BoardModel";
-import { BoardRepo, CreateBoardModel, FindBoardByOwnerIdModel } from "../../../../domain/usecases/board";
+import { BoardRepo, CreateBoardModel, FindBoardByOwnerIdModel, UpdateBoardModel } from "../../../../domain/usecases/board";
 import client from "../client";
 
 export class BoardRepository implements BoardRepo {
- 
+
   async create({ ownerId, slug, title, description }: CreateBoardModel): Promise<BoardModel> {
     const actualDate = new Date();
     return await client.board.create({
@@ -22,6 +22,17 @@ export class BoardRepository implements BoardRepo {
   async findByOwnerId({ ownerId }: FindBoardByOwnerIdModel): Promise<BoardModel[]> {
     return await client.board.findMany({
       where: { ownerId }
+    });
+  }
+
+  async update({ id, ...changes }: UpdateBoardModel): Promise<BoardModel> {
+    const actualDate = new Date();
+    return await client.board.update({
+      where: { id },
+      data: {
+        ...changes,
+        updatedAt: actualDate,
+      }
     });
   }
 
