5d4a24b10f705768c746bf60f5901a064d9c13ee
Franklin <franklinteixeira205@gmail.com>
2023-07-21 08:44:20 -0300

    feat: dto board from array

--- src/app/dtos/BoardDTO.ts
+++ src/app/dtos/BoardDTO.ts
@@ -1,7 +1,12 @@
 import { BoardModel } from "../../domain/models/BoardModel";
 
 export class BoardDTO {
+
   static from({ id, title, slug, description, active, createdAt, updatedAt }: BoardModel) {
     return { id, title, slug, description, active, createdAt, updatedAt };
+  }
+  
+  static fromArray(boards: BoardModel[]) {
+    return boards.map((board) => this.from(board));
   }
 }
